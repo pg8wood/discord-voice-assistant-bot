b5f2889b4a55d9ed2276849eb1a4161b37f0f259
Patrick Gatewood <pg8wood@vt.edu>
2018-10-21 18:56:46 -0400

    Create Dialogflow fulfillment handler

--- index.py
+++ index.py
@@ -11,9 +11,22 @@ from sanic.response import json
 app = Sanic()
 
 @app.route('/')
-async def test(request):
+async def run(request):
     users = await bot.get_online_users()
-    return json({users[0]})
+    reply = {
+        "fulfillmentText": users[0]
+    }
+    return json(reply)
+
+
+@app.route('/get_discord_voice_channel_status', methods=['POST'])
+async def get_discord_voice_channel_status(self):
+    users = await bot.get_online_users()
+    reply = {
+        "fulfillmentText": users[0]
+    }
+    return json(reply)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='127.0.0.1', port=8000)
