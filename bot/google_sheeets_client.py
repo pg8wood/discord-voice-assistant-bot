a8d925c889adb6caa813a94487ca6a135e7332ab
Patrick Gatewood <pg8wood@vt.edu>
2018-12-03 22:51:46 -0500

    Add custom response logic
    Grab triggers and custom responses from Google Sheets

--- bot/google_sheeets_client.py
+++ bot/google_sheeets_client.py
@@ -13,15 +13,24 @@ class GoogleSheetsClient:
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
         credentials = ServiceAccountCredentials.from_json_keyfile_name("./secret/google_sheets_secret.json", scope)
         client = gspread.authorize(credentials)
-        self.command_channel_sheet = client.open("Discord Assistant Bot").sheet1
-        self.command_channel_records = self.command_channel_sheet.get_all_records()
+        master_sheet = client.open("Discord Assistant Bot")
+        command_channel_sheet = master_sheet.sheet1
+        custom_response_sheet = master_sheet.worksheet("Custom Responses")
+
+        self.command_channel_records = command_channel_sheet.get_all_records()
+        self.custom_response_records = custom_response_sheet.get_all_records()
 
     def get_command_channel_id(self, server_id):
         for entry in self.command_channel_records:
             if str(entry["server_id"]) == server_id:
                 return entry["command_channel_id"]
 
-        return None
+    def get_custom_response(self, text):
+        for entry in self.custom_response_records:
+            trigger_phrases = entry["trigger_phrases"].split(",")
+
+            if text in trigger_phrases:
+                return entry["response"]
 
     def is_command_channel(self, text_channel, server_id):
         for entry in self.command_channel_records:
