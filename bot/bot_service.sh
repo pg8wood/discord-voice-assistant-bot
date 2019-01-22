5dee6c278f5554db71f1243697ee9063e6707195
Patrick Gatewood <pg8wood@vt.edu>
2019-01-22 17:37:09 -0500

    Start virtualenv in startup script

--- bot/bot_service.sh
+++ bot/bot_service.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Run the bot and restart if it crashes
+
+# Setup virtualenv 
+source ../python-3.6-env/bin/activate
+
+# Run bot loop
 while true
 do
 	python3 index.py
@@ -7,7 +12,7 @@ do
 
 	if [ $exit_code -ne 0 ] 
 	then 
-		printf "The bot crashed with exit code $exit_code. restarting...\n\n"
+		printf "The bot crashed with exit code $exit_code. Restarting...\n\n"
 		sleep 1
 	else
 		printf "The bot exited gracefully. Bye!\n"
