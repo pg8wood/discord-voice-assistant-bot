88d802db1aa86394a11d5c38d0f3d34593c68881
Benjamin Whipkey <whipkey.benjamin@gmail.com>
2019-03-08 23:24:26 -0500

    Cleaned up installer

--- install.py
+++ install.py
@@ -5,8 +5,8 @@ import os
 
 def install():
 	if(sys.version_info[0] < 3 and sys.version_info[1] != 6):
-		print("Must be using Python 3.6!")
-		sys.exit()
+		print("Must be using Python 3.6! Exiting installer...")
+		sys.exit(1)
 	
 	print("\nInstalling virtualenv...\n")
 	subprocess.call([sys.executable, "-m", "pip", "install", "virtualenv"])
@@ -15,90 +15,56 @@ def install():
 	release = str(platform.release())
 	print("OS info:", system, release)
 	
-	if(system == "Windows"):
-		##install the Windows way
+	venv_directory = ""
+	token_dir_path = ""
+	token_file_path = ""
 	
+	
+	##Set up directories for each OS
+	if(system == "Windows"):
 		venv_directory = "C:\\Users\\" + str(os.getlogin()) + "\\AppData\\Local\\Programs\\Python\\Python" + str(sys.version_info[0]) + str(sys.version_info[1]) + "\\Lib\\venv\\scripts\\nt\\activate.bat"
-		
-		print("Activating venv...")
-		os.startfile(venv_directory)
-		
-		print("Installing dependencies...")
-		installDeps()
-		
-		print("Making directory for Discord token...")
-		token_path = "bot\\secret"
-		if(os.path.isdir(token_path)):
-			print("Token path already exists! Continuing...")
-		else:
-			os.mkdir(token_path)
-			
-		token_path = token_path + "\\token.txt"  ##why use two variables when one will do?
-			
-		if(os.path.isfile(token_path)):
-			overwrite_token = input("Token file already exists! Would you like to overwrite it? (Y/N): ")
-			
-			if(overwrite_token.lower() == "y"):
-				token = input("Enter your bot's Discord token: ")
-				token_file = open(token_path, "w+")
-				token_file.write(token)
-				token_file.close()
-				print("Token saved!")
-			else:
-				print("Using existing token")
-		else:
-			token = input("Enter your bot's Discord token: ")
-			token_file = open(token_path, "w+")
-			token_file.write(token)
-			token_file.close()
-			print("Token saved!")
-		
+		token_dir_path = "bot\\secret"
+		token_file_path = token_dir_path + "\\token.txt"
 		
 	elif(system == "Linux"):
-		##install the Linux way
-		
 		venv_directory = "/usr/lib/python3" + str(sys.version_info[0]) + str(sys.version_info[1]) + "venv/scripts/common/activate"
+		token_dir_path = "bot/secret"
+		token_file_path = token_dir_path + "/token.txt"
 		
-		print("venv_directory:", venv_directory)
+	else:
+		##TODO: add Mac support
+		print("MacOS not yet supported! Exiting installer...")
+		sys.exit(2)
+	
+	print("Activating venv...")
+	os.startfile(venv_directory)
+	
+	print("Installing dependencies...")
+	installDeps()
+	
+	print("Making directory for Discord token...")
+	if(os.path.isdir(token_dir_path)):
+		print("Token path already exists! Continuing...")
+	else:
+		os.mkdir(token_dir_path)
 		
-		print("Activating venv...")
-		os.system(venv_directory)
+	if(os.path.isfile(token_file_path)):
+		overwrite_token = input("Token file already exists! Would you like to overwrite it? (Y/N): ")
 		
-		print("Installing dependencies...")
-		installDeps()
-		
-		print("Making directory for Discord token...")
-		token_path = "bot/secret"
-		if(os.path.isdir(token_path)):
-			print("Token path already exists! Continuing...")
-		else:
-			os.mkdir(token_path)
-			
-		token_path = token_path + "/token.txt"  ##why use two variables when one will do?
-			
-		if(os.path.isfile(token_path)):
-			overwrite_token = input("Token file already exists! Would you like to overwrite it? (Y/N): ")
-			
-			if(overwrite_token.lower() == "y"):
-				token = input("Enter your bot's Discord token: ")
-				token_file = open(token_path, "w+")
-				token_file.write(token)
-				token_file.close()
-				print("Token saved!")
-			else:
-				print("Using existing token")
-		else:
+		if(overwrite_token.lower() == "y"):
 			token = input("Enter your bot's Discord token: ")
-			token_file = open(token_path, "w+")
+			token_file = open(token_file_path, "w+")
 			token_file.write(token)
 			token_file.close()
 			print("Token saved!")
-		
-		
-	else:  ##Not Windows or Linux, must be a Mac
-		##TODO: install the Mac way
-		print("MacOS not yet supported! Oops...")
-	
+		else:
+			print("Using existing token")
+	else:
+		token = input("Enter your bot's Discord token: ")
+		token_file = open(token_file_path, "w+")
+		token_file.write(token)
+		token_file.close()
+		print("Token saved!")
 	
 	print("Success! The bot has been installed.")
 	
